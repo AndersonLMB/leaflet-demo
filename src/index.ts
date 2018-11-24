425de3b1a5eab59d143343db445e45461d26bc6c
lmb <1603497362@qq.com>
2018-11-24 18:22:58 +0800

    commit
    Signed-off-by: lmb <1603497362@qq.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,58 @@
 import { Circle } from "./Shape";
 
+import { Map, TileLayer, Projection } from 'leaflet';
 
 let cir = new Circle(7);
 
-console.log(cir.area());
+
+
+
+var map = new Map(
+    document.getElementById("map"),
+    {
+        center: [0, 0],
+        zoom: 1
+    }
+);
+
+var tdtLabelLayer = new TileLayer(
+    // 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    "https://t{s}.tianditu.gov.cn/DataServer?T=cva_w&x={x}&y={y}&l={z}",
+    {
+        subdomains: ['0', '1', '2', '3', '4', '5', '6', '7']
+    }
+);
+var tdtVecLayer = new TileLayer(
+    // 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    "https://t{s}.tianditu.gov.cn/DataServer?T=vec_w&x={x}&y={y}&l={z}",
+    {
+        subdomains: ['0', '1', '2', '3', '4', '5', '6', '7'],
+
+
+    }
+);
+
+map.addLayer(tdtVecLayer);
+map.addLayer(tdtLabelLayer);
+
+window.navigator.geolocation.getCurrentPosition(
+    (a) => {
+        // console.log('success');
+        // alert(`${a.coords.latitude} ${a.coords.longitude}`);
+        console.log(a);
+        map.flyTo([a.coords.latitude, a.coords.longitude]);
+
+    },
+    (b) => {
+        // console.log('error');
+        // alert('error');
+        console.log(b);
+    },
+    {}
+);
+var app = {
+    "leaflet": L,
+    "map": map
+};
+window.app = app;
 
